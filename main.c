bbd8ec1f3e8216ab4622df51733475bd4b8f43a6
Eric.K <erickanekiyo@gmail.com>
2025-03-19 14:58:25 -0300

    Fase 0

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "projeto.h"
+#include <stdio.h>
+
+int main(){
+int opcao;
+
+do{
+exibirMenu();
+scanf("%d", &opcao);
+
+if(opcao == 0){}
+else if(opcao == 1){}
+else if(opcao == 2){}
+else if(opcao == 3){}
+else if(opcao == 4){}
+else(printf("opca invalida\n");)
+
+}while (opcao != 0);
+
+}
